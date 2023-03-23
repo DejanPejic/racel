52ea0b86d4ed39d81094b3475adf7527ee30e253
dejan <sarajevskapejic@gmail.com>
2023-03-23 16:01:48 +0100

    ir senzor

---
+++ irs.c
@@ -0,0 +1,67 @@
+// gcc -o irm1 irm1.c l bcm2835 -l lirc_clien t
+// sudo ./irm1
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <bcm2835.h>
+#include <lirc/lirc_client.h>
+
+// LED3 je da DVK512 plocici na P28 sto je pin 20 na BCM-u,
+// ako se koristi protobord moze se 
+// koristiti isti ovaj pin ili neki drugi
+#define PIN 20
+
+int main(int argc, char **argv)
+{
+	struct lirc_config *config;
+	char *code;
+	
+	// startuj lirc
+	if(lirc_init("lirc",1)==-1)
+		return 1;
+	
+	if(!bcm2835_init())
+		return 2;
+	
+	// Setuj PIN kao izlazni
+	bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
+
+	// procitaj /etc/lirc/lircrc/lircd.conf
+	if(lirc_readconfig(NULL, &config, NULL) == 0)
+	{
+	// radimo dok je LIRC soket otvoren 0=otvoren -1=zatvoren	
+		while(lirc_nextcode(&code)==0)
+		{
+			//if code=NULL nista nije primljeno - preskoci
+			
+			if(code==NULL) continue;
+			{
+				bcm2835_delay(400);
+				if(strstr(code, "KEY_0"))
+				{
+					printf("KEY_0");
+					
+					// iskljuci
+					bcm2835_gpio_write(PIN, LOW);
+					bcm2835_delay(500);
+				}
+				else if(strstr(code, "KEY_1"))
+				{
+					printf("KEY1\n");
+					
+					// ukljuci
+					bcm2835_gpio_write(PIN, HIGH);
+					bcm2835_delay(500);
+				}
+			}
+			
+			free(code);
+		}
+		
+		lirc_freeconfig(config);
+	}
+	
+	lirc_deinit();
+	bcm2835_close();
+	return 0;
+}
